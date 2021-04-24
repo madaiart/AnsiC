4900eec99f85725ae6bfe3f5724aef9cbbfd2141
Shoel2000 <shoelarteaga@hotmail.com>
2021-04-24 00:19:46 -0500

    1.5. Acabado

--- Chapter 1/Ejercicios1.5.3.c
+++ Chapter 1/Ejercicios1.5.3.c
@@ -3,24 +3,51 @@
    1.5.3 Line Counting
 */
 
-#include <stdio.h>
-/* count lines in input */
-int main()
-{
-    int c, nl;
-    nl = 0;
-    while ((c = getchar()) != EOF)
-        if (c == '\n')
-            ++nl;
-    printf("%d\n", nl);
-    return 0;
-}
+// #include <stdio.h>
+// /* count lines in input */
+// int main()
+// {
+//     int c, nl;
+//     nl = 0;
+//     while ((c = getchar()) != EOF)
+//         if (c == '\n')
+//             ++nl;
+//     printf("%d\n", nl);
+//     return 0;
+// }
 
 /*
 Ejercicio 1-8:
 Escribir un programa que cuente "Espacios en blanco", "tabs", y "nuevas líneas (\n)"
 */
+// #include <stdio.h>
+// int main()
+// {
+//   double c, tabular, espacios, lineas;
 
+//   espacios = 0;
+//   lineas = 0;
+//   tabular = 0;
+//   while ((c = getchar()) != EOF)
+//   {
+//     if (c == '\t')
+//     {
+//       tabular++;
+//     }
+//     if (c == ' ')
+//     {
+//       espacios++;
+//     }
+//     if (c == '\n')
+//     {
+//       lineas++;
+//     }
+//   }
+//   printf("Numero de tabular = %.1f\n", tabular);
+//   printf("Numero de espacios = %.1f\n", espacios);
+//   printf("Numero de lineas %.1f\n", lineas);
+//   return 0;
+// }
 /*
 Ejercicio 1-9:
 Escribir un programa que tome la entrada y reemplace todos los espacios por uno solo.
@@ -28,7 +55,42 @@ Ejemplo:
 > sdf sdfsdf      sdfdf   df dfdfds  -> entrada
   sdf sdfsdf sdfdf df dfdfds         -> salida
 */
+// #include <stdio.h>
+// int main()
+// {
+//   int c, c1;
 
+//   while ((c = getchar()) != EOF)
+//   {
+//     if (c == ' ' && c1 == ' ')
+//     {
+//     }
+//     else
+//     {
+//       c1 = c;
+//       putchar(c);
+//     }
+//   }
+
+//   return 0;
+// }
+// VER 2
+// #include <stdio.h>
+// int main()
+// {
+//   int c, c1;
+
+//   while ((c = getchar()) != EOF)
+//   {
+//     if (c != ' ' || c1 != ' ')
+//     {
+//       c1 = c;
+//       putchar(c);
+//     }
+//   }
+
+//   return 0;
+// }
 /*
 Ejercicio 1-10:
 Escribir un programa que tome la entrada y reemplace tabulacion por \t, espacio por \b y slash por \\
@@ -36,3 +98,32 @@ Ejemplo:
 > sdf sdfsdf      sdfdf   df dfdfds \  -> entrada
   sdf\bsdfsdf\tsdfdf\bdf\bdfdfds\\     -> salida
 */
+
+#include <stdio.h>
+int main ()
+{
+  double c;
+  while ((c= getchar())!=EOF)
+  {
+    if (c == '\t')
+    {
+      putchar('\\');
+      putchar('t');
+    }
+    else if (c== ' ')
+    {
+      putchar('\\');
+      putchar('b');
+    }
+    else if (c== '\\')
+    {
+      putchar('\\');
+      putchar('\\');
+    }
+    else{
+      putchar(c);
+    }
+    
+  }
+   return 0;
+}
--- Chapter 1/Ejercicios1.5.4.c
+++ Chapter 1/Ejercicios1.5.4.c
@@ -3,43 +3,68 @@
    1.5.4 Word Count
 */
 
-#include <stdio.h>
-#define IN 1  /* inside a word */
-#define OUT 0 /* outside a word */
-/* count lines, words, and characters in input */
-int main()
-{
-    int c, nl, nw, nc, state;
-    state = OUT;
-    nl = nw = nc = 0;
-    while ((c = getchar()) != EOF)
-    {
-        ++nc;
-        if (c == '\n')
-            ++nl;
-        if (c == ' ' || c == '\n' || c = '\t')
-            state = OUT;
-        else if (state == OUT)
-        {
-            state = IN;
-            ++nw;
-        }
-    }
-    printf("%d %d %d\n", nl, nw, nc);
-    return 0;
-}
+// #include <stdio.h>
+// #define IN 1  /* inside a word */
+// #define OUT 0 /* outside a word */
+// /* count lines, words, and characters in input */
+// int main()
+// {
+//     int c, nl, nw, nc, state;
+//     state = OUT;
+//     nl = nw = nc = 0;
+//     while ((c = getchar()) != EOF)
+//     {
+//         ++nc;
+//         if (c == '\n')
+//             ++nl;
+//         if (c == ' ' || c == '\n' || c = '\t')
+//             state = OUT;
+//         else if (state == OUT)
+//         {
+//             state = IN;
+//             ++nw;
+//         }
+//     }
+//     printf("%d %d %d\n", nl, nw, nc);
+//     return 0;
+// }
 
 /*
 Ejercicio 1-11:
 Probar el programa, que errores tiene?
 */
 
-
+// #include <stdio.h>
+// #define IN 1  /* inside a word */
+// #define OUT 0 /* outside a word */
+// /* count lines, words, and characters in input */
+// int main()
+// {
+//     int c, nl, nw, nc, state;
+//     state = OUT;
+//     nl = nw = nc = 0;
+//     while ((c = getchar()) != EOF)
+//     {
+//         ++nc;
+//         if (c == '\n')
+//             ++nl;
+//         if (c == ' ' || c == '\n' || c == '\t')
+//             state = OUT;
+//         else if (state == OUT)
+//         {
+//             state = IN;
+//             ++nw;
+//         }
+//     }
+//     printf("Lineas: %d\nPalabras: %d\nCaracteres: %d\n", nl, nw, nc);
+//     return 0;
+// }
 /*
 Ejercicio 1-12:
 Escribir un programa que imprima su entrada una palabra por línea.
 Ejemplo:
 > Hola soy un programa de ejemplo -> entrada
+> Hola\nsoy\nun\nprograma\nde\nejemplo -> entrada
 Hola
 soy
 un
@@ -49,3 +74,22 @@ ejemplo
 
 */
 
+#include <stdio.h>
+int main()
+{
+    double c;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ')
+        {
+        printf("%c", 7);
+         putchar(10);
+
+        }
+        else
+        {
+            putchar(c);
+        }
+    }
+        return 0;
+}
