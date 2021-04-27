e11260fe3d354356435488addc4ff4be22f10879
Shoel2000 <shoelarteaga@hotmail.com>
2021-04-26 23:23:52 -0500

    Se va todo 1,-1,7

--- Chapter 1/Ejercicios1.7.c
+++ Chapter 1/Ejercicios1.7.c
@@ -2,27 +2,49 @@
    1.6 Functions
 */
 
-#include <stdio.h>
-int power(int m, int n);
-/* test power function */
-int main()
-{
-    int i;
-    for (i = 0; i < 10; ++i)
-        printf("%d %d %d\n", i, power(2, i), power(-3, i));
-    return 0;
-}
-/* power: raise base to n-th power; n >= 0 */
-int power(int base, int n)
-{
-    int i, p;
-    p = 1;
-    for (i = 1; i <= n; ++i)
-        p = p * base;
-    return p;
-}
-
+// #include <stdio.h>
+// int power(int m, int n);
+// /* test power function */
+// int main()
+// {
+//     int i;
+//     for (i = 0; i < 10; ++i)
+//         printf("%d %d %d\n", i, power(2, i), power(-3, i));
+//     return 0;
+// }
+// /* power: raise base to n-th power; n >= 0 */
+// int power(int base, int n)
+// {
+//     int i, p;
+//     p = 1;
+//     for (i = 1; i <= n; ++i)
+//         p = p * base;
+//     return p;
+// }
 
 /* Ejercicio 1-15
     Reescribir el programa de conversion de temperatura y convertirlo en función.
 */
+
+#include <stdio.h>
+int temperatura(int menor, int mayor, int salto)
+{
+    int C, F;
+    F = menor;
+    printf("F\t  C\n");
+    while (F <= mayor)
+    {
+        C = 5 * (F - 32) / 9;
+        printf("%d\t%d\n", F, C);
+        F = F + salto;
+    }
+}
+
+int main()
+{
+    int menor = 0, mayor = 30, salto = 2;
+    temperatura(menor, mayor, salto);
+    return 0;
+}
+
+// 
